c4087a724c3a2ac9a1ce05e14febdc4d8f8530e9
Brett <brett84c@gmail.com>
2020-09-03 12:17:26 -0400

    updated port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 const publicPath = path.join(__dirname, 'client');
 
 app.use(express.static(publicPath));
