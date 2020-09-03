b1840ebb5d4ecfc6be74e48ee88a6a57f347f7dd
Brett <brett84c@gmail.com>
2020-09-03 12:03:45 -0400

    second commit

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.port || 8080;
+const publicPath = path.join(__dirname, 'client');
 
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(publicPath));
+
+app.get('/galaga', (req, res)=>{
+  res.sendFile(publicPath + '/galaga');
+})
 
 app.listen(port, ()=>{
   console.log('listening at port:', port);
